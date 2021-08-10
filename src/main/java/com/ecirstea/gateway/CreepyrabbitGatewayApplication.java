917d48c3095f76e0234c797ddb03e8cbf7c2c738
elena <elender1230@gmail.com>
2021-08-10 18:53:13 +0200

    Initial commit

---
+++ src/main/java/com/ecirstea/gateway/CreepyrabbitGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.ecirstea.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CreepyrabbitGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreepyrabbitGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecirstea/gateway/CreepyrabbitGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecirstea.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreepyrabbitGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
